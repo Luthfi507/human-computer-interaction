72d0b2b4fdcbedef2a64dd96be6a3e7cc9968c84
Luthfi507 <luthfialbadar@gmail.com>
2026-08-12 23:08:03 +0700

    feat: enhance hand tracking with pinch detection and add hand landmarking functionality

--- test.py
+++ test.py
@@ -1,52 +1,68 @@
 from time import time
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
 import mediapipe as mp
 from mediapipe.tasks.python.vision import (
-    drawing_utils, drawing_styles,
     FaceDetectorOptions, FaceDetector
 )
 
 from tools.filters import FILTERS
+from utils.hand_tracking import PinchController, HAND_DETECTOR, is_pinch, draw_hand_landmarks
 
 opt = FaceDetectorOptions(base_options=mp.tasks.BaseOptions(model_asset_path="models/blaze_face_full_range.tflite"))
 detector = FaceDetector.create_from_options(opt)
+
+def process_face(frame, face_results, current_filter):
+    for result in face_results.detections:
+        bboxes = result.bounding_box
+
+        x1, y1 = bboxes.origin_x, bboxes.origin_y
+        x2, y2 = bboxes.origin_x + bboxes.width, bboxes.origin_y + bboxes.height
+
+        # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        roi = frame[y1:y2, x1:x2]
+        frame[y1:y2, x1:x2] = FILTERS[current_filter](roi)
+
+    return frame
+
+def process_hands(frame, hand_results):
+    pinching = False
+
+    for landmarks in hand_results.hand_landmarks:
+        draw_hand_landmarks(frame, landmarks)
+
+        if is_pinch(landmarks):
+            pinching = True
+
+    return pinching
 
 def main():
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         raise RuntimeError("No camera detected")
 
-    last_filter_time = time()
-    current_filter = 1
+    current_filter = 0
+    pinch_controller = PinchController()
 
     while True:
         ok, frame = cap.read()
         if not ok:
             break
 
-        current_time = time()
-        if current_time - last_filter_time >= 1:
-            current_filter = (current_filter + 1) % len(FILTERS)
-            last_filter_time = current_time
-
         frame = cv2.flip(frame, 1)
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         mp_image = mp.Image(mp.ImageFormat.SRGB, rgb)
-        results = detector.detect(mp_image)
+        face_results = detector.detect(mp_image)
+        hand_results = HAND_DETECTOR.detect(mp_image)
 
-        for result in results.detections:
-            bboxes = result.bounding_box
+        pinching = process_hands(frame, hand_results)
+        if pinch_controller.update(pinching):
+            current_filter = (
+                current_filter + 1
+            ) % len(FILTERS)
 
-            x1, y1 = bboxes.origin_x, bboxes.origin_y
-            x2, y2 = bboxes.origin_x + bboxes.width, bboxes.origin_y + bboxes.height
-
-            # cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-            filter_img = frame[y1:y2, x1:x2]
-            filter_img = FILTERS[current_filter](filter_img)
-            frame[y1:y2, x1:x2] = filter_img
+        frame = process_face(frame, face_results, current_filter)
 
         cv2.imshow("", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
---
+++ utils/hand_tracking.py
@@ -0,0 +1,57 @@
+from time import time
+import mediapipe as mp
+from mediapipe.tasks.python.vision import (
+    drawing_utils, drawing_styles,
+    HandLandmarker, HandLandmarksConnections, HandLandmarkerOptions,
+)
+
+model_path = 'models/hand_landmarker.task'
+hand_opt = HandLandmarkerOptions(
+    base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
+    num_hands=2,
+    min_hand_detection_confidence=0.6,
+    min_hand_presence_confidence=0.6,
+    min_tracking_confidence=0.6
+)
+HAND_DETECTOR = HandLandmarker.create_from_options(hand_opt)
+
+def draw_hand_landmarks(frame, hand_landmarks):
+    drawing_utils.draw_landmarks(
+        frame,
+        hand_landmarks,
+        HandLandmarksConnections.HAND_CONNECTIONS,
+        drawing_utils.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2),
+        drawing_utils.DrawingSpec(color=(255, 0, 0), thickness=2)
+    )
+
+def is_pinch(landmarks, threshold=0.05):
+    thumb_tip = landmarks[4]
+    index_tip = landmarks[8]
+    distance = ((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2) ** 0.5
+    return distance <= threshold
+
+class PinchController:
+    def __init__(self, max_durations=1.0):
+        self.max_durations = max_durations
+        self.start_time = None
+        self.was_pinching = False
+
+    def update(self, is_pinching: bool):
+        current_time = time()
+        triggered = False
+
+        # pinch is started
+        if is_pinching and not self.was_pinching:
+            self.start_time = current_time
+
+        elif not is_pinching and self.was_pinching:
+            if self.start_time is not None:
+                duration = current_time - self.start_time
+
+                if duration < self.max_durations:
+                    triggered = True
+
+            self.start_time = None
+
+        self.was_pinching = is_pinching
+        return triggered
