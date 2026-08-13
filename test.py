6a9cf152e992c187b004fdb36b159b7c856f334d
Luthfi507 <luthfialbadar@gmail.com>
2026-08-13 11:44:20 +0700

    feat: implement hand polygon processing

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
-from time import time
 import cv2
 import numpy as np
-
 import mediapipe as mp
 from mediapipe.tasks.python.vision import (
     FaceDetectorOptions, FaceDetector
 )
 
 from tools.filters import FILTERS
-from utils.hand_tracking import PinchController, HAND_DETECTOR, is_pinch, draw_hand_landmarks
+from utils.hand_tracking import HandController, HAND_DETECTOR, draw_hand_landmarks
 
+hand_controller = HandController()
 opt = FaceDetectorOptions(base_options=mp.tasks.BaseOptions(model_asset_path="models/blaze_face_full_range.tflite"))
 detector = FaceDetector.create_from_options(opt)
 
@@ -26,16 +25,45 @@ def process_face(frame, face_results, current_filter):
 
     return frame
 
-def process_hands(frame, hand_results):
+def process_pinch(frame, hand_results):
     pinching = False
 
     for landmarks in hand_results.hand_landmarks:
         draw_hand_landmarks(frame, landmarks)
-
-        if is_pinch(landmarks):
-            pinching = True
+        pinching = hand_controller.update(landmarks)
 
     return pinching
+
+def process_polygon(frame, hand_results, current_filter):
+    points = hand_controller.get_poly_point(
+        hand_results,
+        frame.shape
+    )
+
+    if points is None:
+        return frame
+
+    x, y, w, h = cv2.boundingRect(points)
+
+    roi = frame[y:y+h, x:x+w]
+    filtered_roi = FILTERS[current_filter](roi.copy())
+
+    local_points = points - np.array([x, y])
+    mask = np.zeros((h, w), dtype=np.uint8)
+
+    cv2.fillPoly(mask, [local_points], 255)
+
+    mask_3ch = cv2.cvtColor(
+        mask, cv2.COLOR_GRAY2BGR
+    ).astype(np.float32) / 255.0
+
+    blended = (
+        filtered_roi * mask_3ch + roi * (1 - mask_3ch)
+    ).astype(np.uint8)
+
+    frame[y:y+h, x:x+w] = blended
+
+    return frame
 
 def main():
     cap = cv2.VideoCapture(0)
@@ -43,7 +71,6 @@ def main():
         raise RuntimeError("No camera detected")
 
     current_filter = 0
-    pinch_controller = PinchController()
 
     while True:
         ok, frame = cap.read()
@@ -56,13 +83,14 @@ def main():
         face_results = detector.detect(mp_image)
         hand_results = HAND_DETECTOR.detect(mp_image)
 
-        pinching = process_hands(frame, hand_results)
-        if pinch_controller.update(pinching):
+        pinching = process_pinch(frame, hand_results)
+        if pinching:
             current_filter = (
                 current_filter + 1
             ) % len(FILTERS)
 
-        frame = process_face(frame, face_results, current_filter)
+        # frame = process_face(frame, face_results, current_filter - 1)
+        frame = process_polygon(frame, hand_results, current_filter)
 
         cv2.imshow("", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
--- utils/hand_tracking.py
+++ utils/hand_tracking.py
@@ -1,3 +1,4 @@
+import numpy as np
 from time import time
 import mediapipe as mp
 from mediapipe.tasks.python.vision import (
@@ -57,3 +58,42 @@ class HandController:
 
         self.was_pinching = is_pinching
         return triggered
+
+    @staticmethod
+    def landmark_to_pixel(landmark, frame_shape):
+        h, w = frame_shape[:2]
+
+        return (
+            int(landmark.x * w),
+            int(landmark.y * h)
+        )
+
+    def get_poly_point(self, results, frame_shape):
+        if len(results.hand_landmarks) < 2:
+            return
+
+        left_hand = None
+        right_hand = None
+
+        for landmarks, handedness in zip(
+            results.hand_landmarks, results.handedness
+        ):
+            hand_name = handedness[0].category_name
+
+            if hand_name == "Left":
+                left_hand = landmarks
+            elif hand_name == "Right":
+                right_hand = landmarks
+
+        if left_hand is None or right_hand is None:
+            return
+
+        left_thumb = self.landmark_to_pixel(left_hand[4], frame_shape)
+        left_index = self.landmark_to_pixel(left_hand[8], frame_shape)
+        right_thumb = self.landmark_to_pixel(right_hand[4], frame_shape)
+        right_index = self.landmark_to_pixel(right_hand[8], frame_shape)
+
+        points = np.array([
+            left_thumb, left_index, right_index, right_thumb
+        ], dtype=np.int32)
+        return points
