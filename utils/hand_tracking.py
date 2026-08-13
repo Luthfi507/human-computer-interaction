e263be58c1327cabc4a28bda4cbb1e257b99c0fa
Luthfi507 <luthfialbadar@gmail.com>
2026-08-13 10:26:24 +0700

    refactor: consolidate pinch detection into HandController class

--- utils/hand_tracking.py
+++ utils/hand_tracking.py
@@ -24,21 +24,23 @@ def draw_hand_landmarks(frame, hand_landmarks):
         drawing_utils.DrawingSpec(color=(255, 0, 0), thickness=2)
     )
 
-def is_pinch(landmarks, threshold=0.05):
-    thumb_tip = landmarks[4]
-    index_tip = landmarks[8]
-    distance = ((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2) ** 0.5
-    return distance <= threshold
-
-class PinchController:
+class HandController:
     def __init__(self, max_durations=1.0):
         self.max_durations = max_durations
         self.start_time = None
         self.was_pinching = False
 
-    def update(self, is_pinching: bool):
+    def is_pinch(self, landmarks, threshold):
+        thumb_tip = landmarks[4]
+        index_tip = landmarks[8]
+        distance = ((thumb_tip.x - index_tip.x)**2 + (thumb_tip.y - index_tip.y)**2) ** 0.5
+        return distance <= threshold
+
+    def update(self, landmarks, threshold=0.05):
         current_time = time()
         triggered = False
+
+        is_pinching = self.is_pinch(landmarks, threshold)
 
         # pinch is started
         if is_pinching and not self.was_pinching:
